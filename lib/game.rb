2792a3a31259cf9af7fbce01f5ea68b66a98504f
Daniel Zhong <44526047+danielzhong124@users.noreply.github.com>
2025-08-30 14:25:30 -0400

    Update game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -3,20 +3,33 @@
 require_relative 'player'
 
 class Game
+  WIDTH = 7
+  HEIGHT = 6
+
   def initialize
-    @grid = Array.new(6) { Array.new(7, ' ') }
+    @grid = Array.new(HEIGHT) { Array.new(WIDTH, ' ') }
     @players = [Player.new(:red), Player.new(:yellow)]
     @curr_player_num = rand(2)
   end
 
   def play
-    while @grid[0].any? { |space| space == ' ' }
+    winner = nil
+    until winner || grid_full?
       system 'cls'
-      print_grid
 
-      place_player_disc
+      row, col = drop_player_disc
+      winner = curr_player if four_in_row?(row, col)
+
       switch_player!
     end
+    
+    system 'cls'
+    print_grid
+    puts (winner.nil? ? "It's a draw." : "#{winner.name} wins!")
+  end
+
+  def grid_full?
+    @grid[-1].none? { |space| space == ' ' }
   end
 
   def curr_player
@@ -27,34 +40,45 @@ class Game
     @curr_player_num = 1 - @curr_player_num
   end
 
-  def place_player_disc
-    puts "#{curr_player.name}'s turn."
+  def drop_player_disc
+    print_grid
+    col = curr_player.choose_col(@grid)
 
-    col = curr_player.choose_col
-    until @grid[0][col] == ' '
-      puts 'That column is already full. Try again.'
-      col = curr_player.choose_col
-    end
-
-    drop_disc(col)
-  end
-
-  def drop_disc(col)
-    row = -1
-    row -= 1 until @grid[row][col] == ' '
+    row = 0
+    row += 1 until @grid[row][col] == ' '
     @grid[row][col] = curr_player.disc
 
     [row, col]
   end
 
-  def print_grid
-    row_sep = '|---+---+---+---+---+---+---|'.colorize(:blue)
-    col_sep = '|'.colorize(:blue)
+  def four_in_row?(row, col)
+    return true if count_connects(row, col, [0, -1]) >= 3
+    return true if count_connects(row, col, [-1, 0]) + count_connects(row, col, [1, 0]) >= 3
+    return true if count_connects(row, col, [-1, -1]) + count_connects(row, col, [1, 1]) >= 3
+    return true if count_connects(row, col, [-1, 1]) + count_connects(row, col, [1, -1]) >= 3
 
-    puts row_sep
-    @grid.each do |row|
-      puts "#{col_sep} #{row.join(" #{col_sep} ")} #{col_sep}"
-      puts row_sep
-    end
+    false
+  end
+
+  def count_connects(row, col, dir, connects = 0)
+    return connects if connects >= 3
+
+    row += dir[1]
+    col += dir[0]
+
+    return connects unless row.between?(0, HEIGHT - 1) && col.between?(0, WIDTH - 1)
+    return connects unless @grid[row][col] == curr_player.disc
+
+    count_connects(row, col, dir, connects + 1)
+  end
+
+  def print_grid
+    row_sep = "\n|---+---+---+---+---+---+---|\n".colorize(:blue)
+    col_sep = ' | '.colorize(:blue)
+
+    grid_str = String.new
+    @grid.each { |row| grid_str.prepend('| '.colorize(:blue), row.join(col_sep), ' |'.colorize(:blue), row_sep) }
+
+    puts grid_str.prepend(row_sep)
   end
 end
